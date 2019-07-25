6fa625cd882081ac19d827d1b01c62878008c49a
DomenicoParente <48157618+DomenicoParente@users.noreply.github.com>
2019-07-25 19:49:24 +0200

    Add files via upload

--- Progetto-API.c
+++ Progetto-API.c
@@ -2,60 +2,140 @@
 #include <stdio.h>
 #include <string.h>
 
-#define DEBUG
+#define EBUG
 #define MAX 20
 #define XL 60
 #define N 4
+#define T 10
 
-/*typedef struct{
-	char id_relation[MAX];
-	relation *next;	
+typedef struct{						// dichiarazione strutture
+	char id_rel[MAX];
+	char origin[MAX];
+	struct relation *next_r;	
 }relation;
 
 typedef struct {
-	char id_entity[MAX];
+	char id_ent[MAX];
 	relation *rel;
-}entity;*/
+	struct entity *next_e;
+}entity;
 
-int addent(){
+typedef struct{
+	char type_rel[MAX];
+	char max_dest[XL];
+	int max;
+} elenco_type;
+
+entity *list_e;
+elenco_type ntype[T];
+
+void initiate(){					// inizializzazione strutture
+	list_e= NULL;
+	for(int i=0;i<T;i++){
+		strcpy(ntype[i].type_rel,"null");
+		ntype[i].max=0;
+	}
+}
+
+int addent(char *name_e){
+	entity *p;
+	p=malloc(sizeof(entity));
+	if(list_e == NULL){
+		list_e=p;
+		p->next_e= NULL;
+	}
+	else{
+		p->next_e=list_e;
+		list_e=p;
+	}
+	strcpy(p->id_ent,name_e);
+	p->rel=NULL;
 	return 0;
 }
-int delent(){
+int delent(char *id_ent){
+	/*void libera_lista(Lista_int l)
+{
+   if (l != NULL) {
+    libera_lista(l->next);
+    free(l);
+   }
+}*/
 	return 0;
 }
-int addrel(){
+int addrel(char *id_orig, char *id_dest,char *t_rel){
+	int j=0,k=0,i=0;
+	entity *cursor;
+	entity *dest_found;
+	relation *p;
+	if(list_e != NULL){						// verifica della presenza degli enti
+		cursor= list_e;
+		while(cursor != NULL && (j==0 || k==0)){
+			if(strcmp(cursor->id_ent,id_orig)==0){
+				j=1;
+			}
+			if(strcmp(cursor->id_ent,id_dest)==0){
+				k=1;
+				dest_found=cursor;
+			}
+		cursor=cursor->next_e;	
+		}
+	}
+	if(j==0 || k==0){
+		return 1;
+	}
+	p=malloc(sizeof(relation));
+	if(dest_found->rel == NULL){			// inserimento della relazione nella lista
+		dest_found->rel=p;
+		p-next_r= NULL;
+	}
+	else{
+		p-next_r=dest_found->rel;
+		dest_found->rel=p;
+	}
+	strcpy(p->id_rel,t_rel);
+	strcpy(p->origin,id_orig);
+	while(strcmp(ntype[i].type_rel,"null")!=0 && strcmp(ntype[i].type_rel,id_rel)<0){
+			i++;
+		}
+		strcpy(ntype[i].type_rel,id_rel);
+		
 	return 0;
 }
-int delrel(){
+int delrel(char *id_orig, char *id_dest, char *id_rel){
 	return 0;
 }
 int report(){
+	if(strcmp(ntype[0].type_rel,"null")==0){			// verifica se il numero di relazioni e' 0 
+		printf("none\n");
+	}
 	return 0;
 }
 
 int main(){
 	char inst[XL];
-	char *arg[5];
+	char *arg[N];
 	char *string;
 	int i;
 	char *c= NULL;
+	initiate();							// inizializza strutture dati
 	do{
-		fgets(inst,XL,stdin);											//legge le istruzioni
+		fgets(inst,XL,stdin);			// legge le istruzioni
 		for(i=0;i<XL;i++){
 			if(inst[i]=='\n'){
 				inst[i]='\0';
 				break;
 			}
 		}
-		string=strtok(inst," ");										//separa gli argomenti
+		string=strtok(inst," ");			// separa gli argomenti
 		i=0;
 		arg[i]=string;
-		while(string != NULL && i<4){
-			i++;
+		i++;
+		while(string != NULL && i<N){
 			string=strtok(NULL," ");
 			arg[i]=string;
+			i++;
 		}
-			while(i<4){
+			while(i<N){
 				arg[i]=c;
 				i++;
 			}
@@ -65,7 +145,7 @@ int main(){
 			}
 		#endif
 		
-		if(strcmp(arg[0],"addrel")== 0){								//chiamata funzioni
+		if(strcmp(arg[0],"addrel")== 0){			// chiamata funzioni
 			addrel(arg[1],arg[2],arg[3]);			
 		}
 		if(strcmp(arg[0],"delrel")== 0){
